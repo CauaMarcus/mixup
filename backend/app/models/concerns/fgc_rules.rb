f4bdaea094c5961a4a48fcea1967ce15d8b5fd63
CauaMarcus <caua.cardoso600@gmail.com>
2026-04-08 22:37:40 -0300

    Update fgc_rules.rb
    removed HEX_FORMAT and STATIC_IMAGE_FORMAT from fgc_rules.rb

--- backend/app/models/concerns/fgc_rules.rb
+++ backend/app/models/concerns/fgc_rules.rb
@@ -1,5 +1,3 @@
 module FgcRules
-  HEX_FORMAT = /\A#?(?:[A-F0-9]{3}){1,2}\z/i
   URL_FORMAT = /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
-  STATIC_IMAGE_FORMAT = /\.(png|jpg|jpeg)\z/i
 end
